d21f08956d1ca8c03840ff0d63962d1500748bdd
shafiimam <shafi15-8072@diu.edu.bd>
2021-02-23 20:36:52 +0600

    react state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,111 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-    var person = {
-        name: 'Dr. Mahfuz',
-        job:'singer'
-    };
-    var style = {
-        color: '#FF0000',
-        backgroundColor: 'yellow'
-    }
+    const products = [
+        {name:'photoshop',price: '$90.99'},
+        {name:'ilusstrator',price: '$60.99'},
+        {name:'pdf reader',price: '$6.99'}
+    ]
+    const friends =[
+        {name:'emon',age:'23',job:'marchandizer'},
+        {name:'rana',age:'26',job:'developer'},
+        {name:'rabby',age:'25',job:'freelancer'},
+    ]
+
+    const nayoks=['anwar','jafor iqbal','sakib khan','salman shah','bappy','shuvo'];
+
+    const productNames = products.map(pd=>pd.name);
+    
     return ( 
     <div className = "App" >
         <header className = "App-header" >
-        <img src = { logo }
-        className = "App-logo"
-        alt = "logo"/>
-        <p> Edit <code> src / App.js </code> and save to reload. </p> 
-        <p> my first paragraph </p> 
-        <p>my second paragraph </p>
-        <h1 style={style}>my heading: {person.name + ' ' + person.job}</h1>
-        <h3>singer: {person.name} job: {person.job}</h3>
-        <p>my third paragraph </p>
-        <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React </a> 
+        <p>I am a react person</p>
+        <Counter></Counter>
+        <ul>{
+            nayoks.map(nayok=> <li>{nayok}</li>)
+            }
+        </ul>
+        {
+            products.map(pd=> <Product product={pd}></Product>)
+        }
+
+        {
+            friends.map(frnd=> <Friend friend={frnd}></Friend>)
+        }
+        <Person name = "piash" profession="web developer"></Person>
+        <Person name = "priya" profession="house wife"></Person>
+        <Person name="rafi" profession="jobless"></Person>
         </header> 
     </div>
     );
 }
 
+function Counter(){
+    const [count,setCount] = useState(0);
+    
+    return (
+        <div>
+            <h1>count: {count}</h1>
+            <button onClick={()=> setCount(count + 1)}>increase</button>
+            <button onClick={()=> setCount(count - 1)}>decrease</button>
+        </div>
+    )
+}
+function Product(props) {
+    const productStyle = {
+        border: '1px solid gery',
+        borderRadius: '5px',
+        backgroundColor : 'lightgray',
+        height: '250px',
+        width: '250px',
+        float:'left',
+        margin: '5px',
+        boxShadow:'5px 5px 10px white'
+    }
+const {name,price} = props.product;
+    return(
+        <div style={productStyle}>
+            <h2>{name}</h2>
+            <h3>Price:{price}</h3>
+            <button>Buy Now</button>
+        </div>
+    )
+}
+
+function Friend(props){
+    const friendStyle = {
+        height:'250px',
+        width:'200px',
+        margin:'5px',
+        border:'1px solid gray',
+        borderRadius:'5px',
+        backgroundColor: '#dddccc',
+        color: 'black'
+    }
+    const {name,age,job} = props.friend;
+    return (
+        <div style={friendStyle}>
+            <h3>Name:{name}</h3>
+            <h4>Age:{age}</h4>
+            <h6>Job:{job}</h6>
+        </div>
+    )
+}
+
+function Person(props){
+    var style = {
+        border: '2px solid gold',
+        padding: '5px',
+        margin: '5px',
+        width: '400px'
+    }
+    return (
+        <div style={style}>
+            <h3>My name: {props.name}</h3>
+            <p>My profession: {props.profession}</p>
+        </div>
+    )
+}
 export default App;
