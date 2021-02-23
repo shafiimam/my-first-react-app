2a90a16e3d8d6dd6c09fea9f362bc6beb52467c8
shafiimam <shafi15-8072@diu.edu.bd>
2021-02-23 13:28:32 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    var person = {
+        name: 'Dr. Mahfuz',
+        job:'singer'
+    };
+    var style = {
+        color: '#FF0000',
+        backgroundColor: 'yellow'
+    }
+    return ( 
+    <div className = "App" >
+        <header className = "App-header" >
+        <img src = { logo }
+        className = "App-logo"
+        alt = "logo"/>
+        <p> Edit <code> src / App.js </code> and save to reload. </p> 
+        <p> my first paragraph </p> 
+        <p>my second paragraph </p>
+        <h1 style={style}>my heading: {person.name + ' ' + person.job}</h1>
+        <h3>singer: {person.name} job: {person.job}</h3>
+        <p>my third paragraph </p>
+        <a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React </a> 
+        </header> 
     </div>
-  );
+    );
 }
 
 export default App;
